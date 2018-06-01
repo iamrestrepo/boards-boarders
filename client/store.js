ef947dc1f38fe4eaeefe8f6c5e6267e7de68fcd8
Monique <moniquerz@Moniques-MacBook-Pro.local>
2018-06-01 15:39:48 -0400

    sets up redux dev tools in store,adds passport and express sessions

--- client/store.js
+++ client/store.js
@@ -2,13 +2,12 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers/root";
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
-// import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancer(applyMiddleware(thunkMiddleware, createLogger()))
+  composeEnhancers(applyMiddleware(thunkMiddleware, createLogger()))
 );
 
 export default store;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const session = require("express-session"); // sessions allow us to keep user autheticated even after refreshing their browser window
 const app = express();
 const morgan = require("morgan");
 const path = require("path");
 const apiRoutes = require("./api");
+const db = require("./database/db");
+const passport = require("passport");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const dbStore = new SequelizeStore({ db: db });
+
+dbStore.sync();
 
 //logging middleware
 
@@ -14,6 +22,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // The “extended” syntax allows for rich objects and arrays to be encoded into the URL-encoded format
 
 app.use("api", apiRoutes);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "Some insecurely saved secret", // the process.env.SESSION_SECRET allows us to use the secret from the enviroment variable instead of exposing our own secret
+    resave: false,
+    saveUninitialized: false,
+    store: dbStore
+    // duration: -> how long willthe session be alive for
+    // activeDuration: INT -> before logging an user out, it allows INT extra time if require by the user
+  })
+);
+
+app.use(passport.initialize()); // We initilize passport so that it will consume our req.session object, and attach the user to the request object.
+app.use(passport.session());
 
 app.get("*", (req, res, next) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
