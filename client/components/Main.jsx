6c78387b4e2052b39ecdd4030b0fba911c393f12
Monique <moniquerz@Moniques-MacBook-Pro.local>
2018-06-12 10:23:46 -0400

    adds hashroutes to main

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import Board from "./Board.jsx";
+import Rider from "./Rider.jsx";
+import Maker from "./Maker.jsx";
+import Navbar from "./basicComponents/Navbar.jsx";
+import LandingPage from "./LandingPage.jsx";
 
 export default class Main extends Component {
   render() {
-    return <h1> Boards and Riders </h1>;
+    return (
+      <div className="main-wrapper">
+        <div>
+          <Navbar />
+        </div>
+        <div className="main-wrapper">
+          <Switch>
+            <Route path="/" exact component={LandingPage} location={location} />
+            <Route exact path="/board" component={Board} />
+            <Route exact path="/maker" component={Maker} />
+            <Route exact path="/rider" component={Rider} />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
