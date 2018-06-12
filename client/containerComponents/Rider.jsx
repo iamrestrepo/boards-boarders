f4f1a4648f687be525b827ab681574320a884e6b
Monique <moniquerz@Moniques-MacBook-Pro.local>
2018-06-12 17:07:45 -0400

    retrieves all data from server through DisplayInfoAsList component fetching from container components

--- client/containerComponents/Rider.jsx
+++ client/containerComponents/Rider.jsx
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import DisplayInfoAsList from "../presentationalComponents/DisplayInfoAsList.jsx";
+import { goGetRidersFromServer } from "../reducers/riderReducer";
 
-export default class Rider extends Component {
+export class Rider extends Component {
+  componentDidMount() {
+    this.props.getRiders();
+  }
   render() {
-    return <h1> Rider </h1>;
+    const riders = this.props.riders;
+    if (riders < 1) {
+      return <h3>loading...</h3>;
+    } else {
+      return <DisplayInfoAsList actualInfo={riders} />;
+    }
   }
 }
+
+const mapStatetoProps = state => ({
+  riders: state.riders
+});
+
+const mapDispatchToProps = dispatch => ({
+  getRiders: () => {
+    dispatch(goGetRidersFromServer());
+  }
+});
+
+export default connect(
+  mapStatetoProps,
+  mapDispatchToProps
+)(Rider);
