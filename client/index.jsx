08eb301878ed9da1e1d1b4940ecfe9d46b367cc9
Monique <moniquerz@Moniques-MacBook-Pro.local>
2018-06-13 22:19:42 -0400

    adds material ui nav bar with working buttons

--- client/index.jsx
+++ client/index.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import "../public/style.css";
+import "../public/css/style.css";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store.js";
 import Main from "./presentationalComponents/Main.jsx";
 import { HashRouter } from "react-router-dom";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import DarkTheme, {
+  darkBaseTheme
+} from "material-ui/styles/baseThemes/darkBaseTheme";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <HashRouter>
-      <Main />
-    </HashRouter>
-  </Provider>,
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <Provider store={store}>
+      <HashRouter>
+        <Main />
+      </HashRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById("app")
 );
--- client/presentationalComponents/Navbar.jsx
+++ client/presentationalComponents/Navbar.jsx
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
+import AppBar from "material-ui/AppBar";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
-    return <h1> Nav Bar </h1>;
+    return (
+      <AppBar position="static" color="primary">
+        <Button color="inherit" href="#/board">
+          Boards
+        </Button>
+        <Button color="inherit" href="#/maker">
+          Makers
+        </Button>
+        <Button color="inherit" href="#/rider">
+          Riders
+        </Button>
+      </AppBar>
+    );
   }
 }
